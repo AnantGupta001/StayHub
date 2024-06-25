57a0bccc448be323a9ade100313fe2b15a0e480d
AnantGupta001 <anantagarwal4946@gmail.com>
2024-06-25 19:17:44 +0530

    Created Update route for listings

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/StayHub';
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 main()
     .then((res) => {
@@ -38,7 +40,7 @@ app.get("/listings/new", (req,res)=>{
     res.render("listings/new.ejs");
 });
 
-app.post("/listings/new", async (req, res) => {
+app.post("/listings", async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -49,6 +51,19 @@ app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
+});
+
+//Edit ROUTE
+app.get("/listings/:id/edit", async (req,res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+app.put("/listings/:id", async (req,res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ... req.body.listing});
+    res.redirect(`/listings/${id}`);
 });
 
 app.listen(8080, () => {
